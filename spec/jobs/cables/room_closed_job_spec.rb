f144fbd1078119823bf564011af526dca376d2bf
somenugget <some.nugget@gmail.com>
2018-09-26 04:39:10 +0300

    #3 Tests for all jobs

--- spec/jobs/cables/room_closed_job_spec.rb
+++ spec/jobs/cables/room_closed_job_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Cables::RoomClosed, type: :job do
   describe 'job' do
     it 'broadcasts message to cable' do
-      expect(RoomChannel).to receive(:broadcast_to).with(
-        1, hash_including(message: :room_closed, data: { average: 1 })
-      )
+      expect(RoomChannel)
+        .to receive(:broadcast_to)
+        .with(1, hash_including(message: :room_closed, data: { average: 1 }))
+
       described_class.perform_now 1, 1
     end
   end
---
+++ spec/jobs/cables/user_voted_job_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Cables::UserVoted, type: :job do
+  describe 'job' do
+    it 'broadcasts message to cable' do
+      allow_any_instance_of(RoomUser::Representer::Json)
+        .to receive(:as_json)
+        .and_return('{"right_json": true}')
+      expect(RoomChannel)
+        .to receive(:broadcast_to)
+        .with(1, hash_including(message: :user_voted, data: '{"right_json": true}'))
+
+      described_class.perform_now OpenStruct.new(room_id: 1)
+    end
+  end
+end
